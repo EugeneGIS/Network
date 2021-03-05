316804d73c35fddad21c699575aaff69d5df0a75
Haokun Liu <eugeneliuhaokun@gmail.com>
2021-03-05 15:36:39 +0100

    exercise

--- Network_exercise.py
+++ Network_exercise.py
@@ -1,4 +1,4 @@
-#script for analysing betweenness centrality in the road network of the Canton of ZH
+#script for analysing betweenness centrality in the road network of the Canton of ZH1
 import numpy as np
 import pandas as pd
 import networkx as nx
